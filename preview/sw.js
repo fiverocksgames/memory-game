51c4b0e49c83697916b4ca5f00724dcc4a69ddb1
fiverocksgames deployment <163599746+fiverocksgames@users.noreply.github.com>
2026-08-29 13:43:04 +0900

    preview: fiverocks-dev/memory-game@db6d6bf3fef31dd7f22ce32f192a78e35507b0ae

---
+++ preview/sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'memory-game-' + "db6d6bf3fef3";
+const ASSETS = [
+  "/assets/icon-192-B2VyLwMa.png",
+  "/assets/index-D0IPjrA2.js",
+  "/assets/index-DdFqisuU.css",
+  "/assets/manifest-Bp7WjCGY.json",
+  "/icons/icon-144.png",
+  "/icons/icon-192.png",
+  "/icons/icon-36.png",
+  "/icons/icon-48.png",
+  "/icons/icon-512.png",
+  "/icons/icon-72.png",
+  "/icons/icon-96.png",
+  "/index.html",
+  "/manifest.json"
+];
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS.map((asset) => '.' + asset))));
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(Promise.all([
+    self.clients.claim(),
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+  ]));
+});
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+});
