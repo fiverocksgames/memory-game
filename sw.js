d724637448567d89884f93c02c23f1bd90d6096c
fiverocksgames deployment <163599746+fiverocksgames@users.noreply.github.com>
2026-09-08 13:51:14 +0900

    deploy: fiverocks-dev/memory-game@d862326c2b0c3b9450f4a57554fe7f0804dd9841

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'memory-game-' + "148384fee3be";
+const CACHE_NAME = 'memory-game-' + "d862326c2b0c";
 const ASSETS = [
   "/assets/icon-192-B2VyLwMa.png",
-  "/assets/index-BoomgIYp.css",
-  "/assets/index-XeCc8B_M.js",
+  "/assets/index-987q2_t2.js",
+  "/assets/index-DWQgYwLz.css",
   "/assets/manifest-Bp7WjCGY.json",
   "/icons/icon-144.png",
   "/icons/icon-192.png",
